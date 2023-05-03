5cfb6e50ae4bdddcef614bd24d8984ad619c38f2
Kyle <kyle@kxnrl.com>
2023-05-03 23:38:30 +0800

    Fix global/zh server detection

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,11 @@
 ﻿using DiscordRPC;
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Management;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -46,10 +49,8 @@ internal static class Program
 
                 Debug.Print($"InLoop");
 
-                var hndlZh = FindWindow("UnityWndClass", "崩坏：星穹铁道");
-                var hndlEn = FindWindow("UnityWndClass", "Honkai: Star Rail");
-                if (hndlZh == IntPtr.Zero &&
-                    hndlEn == IntPtr.Zero)
+                var handle = FindWindow("UnityWndClass", "崩坏：星穹铁道"); // "Honkai: Star Rail"
+                if (handle == IntPtr.Zero)
                 {
                     Debug.Print($"Not found game process.");
                     playing = false;
@@ -66,69 +67,37 @@ internal static class Program
 
                 try
                 {
-                    var procEn = Process.GetProcesses().FirstOrDefault(x => x.MainWindowHandle == hndlEn);
-                    var procZh = Process.GetProcesses().FirstOrDefault(x => x.MainWindowHandle == hndlZh);
+                    var process = Process.GetProcesses().First(x => x.MainWindowHandle == handle);
 
-                    Debug.Print($"Check process with {hndlZh} | {procZh?.ProcessName} || {hndlEn} | {procEn?.ProcessName}");
+                    var isGlobal = CheckGlobal(process);
 
-                    if (procZh != null)
+                    Debug.Print($"Check process with {handle} | {process.ProcessName}");
+
+                    if (!isGlobal)
                     {
                         if (!playing)
                         {
                             playing = true;
-
-                            clientZh.SetPresence(new RichPresence
-                            {
-                                Assets = new Assets
-                                {
-                                    LargeImageKey = "logo",
-                                    LargeImageText = "崩坏：星穹铁道",
-                                },
-                                Timestamps = Timestamps.Now,
-                            });
-
-                            Debug.Print($"Set RichPresence to {procZh?.ProcessName}");
+                            clientZh.UpdateRpc("logo", "崩坏：星穹铁道");
+                            Debug.Print($"Set RichPresence to {process.ProcessName}");
                         }
                         else
                         {
-                            Debug.Print($"Keep RichPresence to {procZh?.ProcessName}");
-                        }
-                    }
-                    else if (procEn != null)
-                    {
-                        if (!playing)
-                        {
-                            playing = true;
-
-                            clientEn.SetPresence(new RichPresence
-                            {
-                                Assets = new Assets
-                                {
-                                    LargeImageKey = "logo",
-                                    LargeImageText = "Honkai: Star Rail",
-                                },
-                                Timestamps = Timestamps.Now,
-                            });
-
-                            Debug.Print($"Set RichPresence to  {procEn?.ProcessName}");
-                        }
-                        else
-                        {
-                            Debug.Print($"Keep RichPresence to {procEn?.ProcessName}");
+                            Debug.Print($"Keep RichPresence to {process.ProcessName}");
                         }
                     }
                     else
                     {
-                        playing = false;
-                        if (clientEn.CurrentPresence != null)
+                        if (!playing)
                         {
-                            clientEn.ClearPresence();
+                            playing = true;
+                            clientEn.UpdateRpc("logo", "Honkai: Star Rail");
+                            Debug.Print($"Set RichPresence to  {process.ProcessName}");
                         }
-                        if (clientZh.CurrentPresence != null)
+                        else
                         {
-                            clientZh.ClearPresence();
+                            Debug.Print($"Keep RichPresence to {process.ProcessName}");
                         }
-                        Debug.Print($"Clear RichPresence");
                     }
                 }
                 catch (Exception e)
@@ -192,6 +161,76 @@ internal static class Program
         Application.Run();
     }
 
-    [DllImport("user32.dll", EntryPoint = "FindWindow")]
+    private static void UpdateRpc(this DiscordRpcClient client, string key, string text)
+        => client.SetPresence(new RichPresence
+        {
+            Assets = new Assets
+            {
+                LargeImageKey = key,
+                LargeImageText = text,
+            },
+            Timestamps = Timestamps.Now,
+        });
+
+    private static bool CheckGlobal(Process process)
+    {
+        var path = GetPathOfWindow(process);
+        var file = Path.Combine(Path.GetDirectoryName(path)!, "config.ini");
+
+        if (!File.Exists(file))
+            throw new FileNotFoundException("Config not found");
+
+        var value = GetIniSectionValue(file, "General", "cps", "gw_PC");
+
+        return value is not ("gw_PC" or "bilibili_PC");
+    }
+
+    private static string GetIniSectionValue(string file, string section, string key, string defaultVal = null)
+    {
+        var stringBuilder = new StringBuilder(1024);
+        GetPrivateProfileString(section, key, defaultVal, stringBuilder, 1024, file);
+        return stringBuilder.ToString();
+    }
+
+    [DllImport("kernel32.dll")]
+    private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retval, int size, string filePath);
+
+    [DllImport("user32.dll")]
     private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    private static string GetPathOfWindow(Process process)
+    {
+        try
+        {
+            return process.MainModule!.FileName;
+        }
+        catch (Exception e)
+        {
+            Debug.Print(e.ToString());
+        }
+
+        try
+        {
+            using var searcher = new ManagementObjectSearcher("SELECT ProcessId, ExecutablePath, CommandLine FROM Win32_Process");
+            using var collection = searcher.Get();
+            var results = collection.Cast<ManagementObject>();
+
+            foreach (var item in results)
+            {
+                var id = (int)(uint)item["ProcessId"];
+                var path = (string)item["ExecutablePath"];
+
+                if (id == process.Id)
+                {
+                    return path;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Print(e.ToString());
+        }
+
+        throw new InvalidOperationException("Failed to get path of handle");
+    }
 }
